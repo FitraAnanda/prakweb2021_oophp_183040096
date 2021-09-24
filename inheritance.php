d7305c78c54f575338ae311bcc5411f5f9cdaec1
FitraAnanda <fitraanandapku123@gmail.com>
2021-09-24 16:43:38 +0700

    ini merupakan tugas ke-2

--- inheritance.php
+++ inheritance.php
@@ -1,19 +1,21 @@
-<?php 
+<?php
 
 // Jualan Produk
 // Komik
 // Game    
 
-class Produk {
+class Produk
+{
 	public $judul,
-		   $penulis,
-		   $penerbit,
-		   $harga,
-		   $jmlHalaman,
-		   $waktuMain;
-		   
+		$penulis,
+		$penerbit,
+		$harga,
+		$jmlHalaman,
+		$waktuMain;
 
-	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0 ) {
+
+	public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
+	{
 		$this->judul = $judul;
 		$this->penulis = $penulis;
 		$this->penerbit = $penerbit;
@@ -22,37 +24,44 @@ class Produk {
 		$this->waktuMain = $waktuMain;
 	}
 
-	public function getLabel() {
+	public function getLabel()
+	{
 		return "$this->penulis, $this->penerbit";
-	}	
-
-	public function getInfoProduk() {
-		$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
-
-		return $str;
 	}
 
-} 
+	public function getInfoProduk()
+	{
+		$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
 
-
-class Komik extends Produk {
-	public function getInfoProduk() {
-		$str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
 		return $str;
 	}
 }
 
 
-class Game extends Produk {
-	public function getInfoProduk() {
+class Komik extends Produk
+{
+	public function getInfoProduk()
+	{
+		$str = "Komik : " .  parent::getInfoProduk() . "- {$this->jmlHalaman} Halaman.";
+		return $str;
+	}
+}
+
+
+class Game extends Produk
+{
+	public function getInfoProduk()
+	{
 		$str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
 		return $str;
 	}
 }
 
 
-class 	CetakInfoProduk {
-	public function cetak ( Produk $produk ) {
+class 	CetakInfoProduk
+{
+	public function cetak(Produk $produk)
+	{
 		$str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
 		return $str;
 	}
@@ -65,9 +74,9 @@ class 	CetakInfoProduk {
 
 
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0 );
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
 
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50 );
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
 
 
 
