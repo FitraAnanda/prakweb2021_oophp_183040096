ae4e8fa427671e598bd1595459ce934920357bfe
FitraAnanda <fitraanandapku123@gmail.com>
2021-09-24 22:24:36 +0700

    ini merupakan lanjutan kodingan oophp bagian-2 menambahkan constant

---
+++ constant.php
@@ -0,0 +1,16 @@
+<?php
+
+// define('NAMA', 'Fitra Ananda');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 21;
+// echo UMUR;
+
+class Coba
+{
+    const NAMA = 'Fitra Ananda';
+}
+
+echo Coba::NAMA;
